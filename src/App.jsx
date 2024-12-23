ad71eeb0c6ae021b14600cbb41028ea27d7ae633
Muhammad Anas <anassoormo775@gmail.com>
2024-12-23 13:29:54 +0500

    Responsive Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import ProductCard from "./components/ProductCard";
 import Header from "./components/Header";
 import AboutUs from "./Pages/About"
 
+
 function App() {
 
   return (
     <>
-    <Banner/>
+    {/* <div className=/" h-screen bg-red-500 md:bg-green-600">
+
+    </div> */}
+
+
+
+    / <Banner/> 
     <div className="mr-[136px] ml-[136px]">
     <Header />
     <AboutUs />
     <ProductCard />
-    </div>
+    </div> 
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,17 @@ import { CiSearch } from "react-icons/ci";
 import { IoMdHeart } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
 import { LuUserRound } from "react-icons/lu";
+import { IoIosMenu } from "react-icons/io";
+import { useState } from "react";
+import { MdOutlineCancel } from "react-icons/md";
 
-const links = ["Home", "About US", "Contact", ];
-const navlinkStyle = "capitalize hover:underline cursor-pointer hover: text-primary"
+
+const links = ["Home", "About US", "Contact"];
+const navlinkStyle =
+  "capitalize hover:underline cursor-pointer hover:text-primary";
 export default function Header() {
-  const isloggedIn = true;
+  const isloggedIn = false;
+  const [isHamburgerOpen, setIsHamBurgerOpen] = useState(false)
   return (
     <div className="h-[38] mt-10 flex items-center justify-between ">
       <div className="flex items-center">
@@ -21,31 +27,37 @@ export default function Header() {
         />
         <CiSearch />
       </div>
-      <ul className="font-bold pl-52 flex gap-5 ">
+      {/* <ul className="font-bold pl-52 flex gap-5 ">
         {links.map((items, i) => (
-          <li className={navlinkStyle} key={i}> {items}</li>
-          // <li> 
-          //   <a
-          //     className="hover:underline hover:text-primary transition-all duration-700 font-bold"
-          //     href="/"
-          //   >
-          //     {items}
-          //   </a>
-          // </li>
+          <li className={navlinkStyle} key={i}>
+            {" "}
+            {items}
+          </li>
         ))}
-          {!isloggedIn ? <li className={navlinkStyle}>Login</li> : null}
-      </ul>
+        {!isloggedIn ? <li className={navlinkStyle}>Login</li> : null}
+      </ul> */}
+      <button className="  md:hidden text-4xl order-3  " onClick={()=>setIsHamBurgerOpen(!isHamburgerOpen)}>
+    {isHamburgerOpen ? <MdOutlineCancel /> : <IoIosMenu />  }
+      </button>
+
       <div className="icons center text-2xl gap-3">
         {isloggedIn ? (
-        <>
-      <IoMdHeart  className="w-8 h-8" />
-      <IoCartOutline  className=" w-8 h-7"/>
-        </>
-
+          <>
+            <IoMdHeart className="w-8 h-8" />
+            <IoCartOutline className=" w-8 h-7" />
+          </>
         ) : null}
       </div>
-
-      <LuUserRound className=" border text-white bg-primary w-8 h-8" />
+      <div className="rounded-full overflow-hidden text-white bg-primary w-8 h-8">
+      {isloggedIn ? (
+        <img
+        src="https://media.licdn.com/dms/image/v2/D5635AQHqT6pfvG8nHA/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1732688533700?e=1735545600&v=beta&t=UcJi2wcX6LDmVpDel_Okyx_20fp_ch_fFt7bLzWmD5c"
+        alt=""
+        />
+      ) : 
+      <LuUserRound className="  w-8 h-8" />
+    }
+    </div>
     </div>
   );
 }
